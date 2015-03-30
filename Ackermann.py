e5203af53be1cab4ac8de9ed56d4ad111ccb5385
Caleb Levy <caleb.levy@berkeley.edu>
2015-03-29 19:10:28 -0700

    Added test for Ackermann function

--- Ackermann.py
+++ Ackermann.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env Python
+#!/usr/bin/env python
 
 """Representation of the Ackermann function in python.
 
@@ -14,4 +14,6 @@ def Ackermann(m, n):
     elif m > 0 and n > 0:
         return Ackermann(m-1, Ackermann(m, n-1))
 
-print Ackermann(3, 3)
+
+if __name__ == '__main__':
+    assert Ackermann(3, 3) == 61
