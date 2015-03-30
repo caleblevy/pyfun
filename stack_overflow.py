814551963da51009ab98ac219a4a84edb2cee22f
Caleb Levy <caleb.levy@berkeley.edu>
2015-03-29 19:56:49 -0700

    Rewrote stack_overflow to be pathologically pep8

--- stack_overflow.py
+++ stack_overflow.py
@@ -1,33 +1,41 @@
-'''
-Writes a python file that, when executed, will produce a stack overflow by sheer number of function calls.
-'''
+#!/usr/bin/env python
 
-N = 2048
-prog_start = '''a = 2
+"""Writes a python file that, when executed, will produce a stack overflow
+without employing recursion.
+
+Also note that despite some seriously screwey indentation, this program is
+pep8.
+
+Caleb Levy, Spring 2014.
+"""
+
+
+def deep_call(n=2048):
+    program_start = """a = 2
 
 
 def double0(x):
-    return x+x
+    return x + x
 
-'''
-def_list = []
-for I in range(N):
-    string = '''
-def double%s(N):
-    return double%s(N)
+"""
 
-''' %(str(I+1),str(I))
-    def_list.append(string)
+    def_list = []
+    for i in range(n):
+        def_string = """
+def double%s(x):
+    return double%s(x)
 
-prog = [prog_start]
-prog.extend(def_list)
+""" % (str(i+1), str(i))
+        def_list.append(def_string)
 
-prog_end = '''
-b = double%s(a)
-'''%str(I+1)
-prog.append(prog_end)
-prog_text = ''.join(prog)
+    prog = [program_start] + def_list + [
+        """
+
+b = double%s(a)""" % str(i+1)
+        ]
+    prog_text = ''.join(prog)
+    with open('rec.py', 'w') as f:
+        f.write(prog_text)
 
 if __name__ == '__main__':
-    with open('rec.py','w') as f:
-        f.write(prog_text)
+    deep_call()
