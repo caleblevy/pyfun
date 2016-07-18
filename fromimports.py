22e15484ee96f80e5dac4023e9f6988f9fca1ad3
Caleb Levy <cclevy@princeton.edu>
2016-07-18 14:20:52 -0400

    Added fromimports.py

---
+++ fromimports.py
@@ -0,0 +1,276 @@
+"""from everything available in Python 3.5.2"""
+
+# Processing Services
+from string import *
+from re import *
+from difflib import *
+from textwrap import *
+from unicodedata import *
+from stringprep import *
+from readline import *
+from rlcompleter import *
+
+# Binary Data Services
+from struct import *
+from codecs import *
+
+# Data Types
+from datetime import *
+from calendar import *
+from collections import *
+from collections.abc import *
+from heapq import *
+from bisect import *
+from array import *
+from weakref import *
+from types import *
+from copy import *
+from pprint import *
+from reprlib import *
+from enum import *
+
+# Numeric and Mathematical Modules
+from numbers import *
+from math import *
+from cmath import *
+from decimal import *
+from fractions import *
+from random import *
+from statistics import *
+
+# Functional Programming Modules
+from itertools import *
+from functools import *
+from operator import *
+
+# File and Directory Access
+from pathlib import *
+from os.path import *
+from fileinput import *
+from stat import *
+from filecmp import *
+from tempfile import *
+from glob import *
+from fnmatch import *
+from linecache import *
+from shutil import *
+from macpath import *
+
+# Data Persistence
+from pickle import *
+from copyreg import *
+from shelve import *
+from marshal import *
+from dbm import *
+from sqlite3 import *
+
+# Data Compression and Archiving
+from zlib import *
+from gzip import *
+from bz2 import *
+from lzma import *
+from zipfile import *
+from tarfile import *
+
+# File Formats
+from csv import *
+from configparser import *
+from netrc import *
+from xdrlib import *
+from plistlib import *
+
+# Cryptographic Services
+from hashlib import *
+from hmac import *
+
+# Generic Operating System Services
+from os import *
+from io import *
+from time import *
+from argparse import *
+from getopt import *
+from logging import *
+from logging.config import *
+from logging.handlers import *
+from getpass import *
+from curses import *
+from curses.textpad import *
+from curses.ascii import *
+from curses.panel import *
+from platform import *
+from errno import *
+from ctypes import *
+
+# Concurrent Execution
+from threading import *
+from multiprocessing import *
+
+from concurrent.futures import *
+from subprocess import *
+from sched import *
+from queue import *
+from dummy_threading import *
+from _thread import *
+from _dummy_thread import *
+
+# Interprocess Communication and Networking
+from socket import *
+from ssl import *
+from select import *
+from selectors import *
+from asyncio import *
+from asyncore import *
+from asynchat import *
+from signal import *
+from mmap import *
+
+# Internet Data Handling
+from email import *
+from json import *
+from mailcap import *
+from mailbox import *
+from mimetypes import *
+from base64 import *
+from binhex import *
+from binascii import *
+from quopri import *
+from uu import *
+
+# Structured Markup Processing Tools
+from html import *
+from html.parser import *
+from html.entities import *
+# XML Processing Modules
+from xml.etree.ElementTree import *
+from xml.dom import *
+from xml.dom.minidom import *
+from xml.dom.pulldom import *
+from xml.sax import *
+from xml.sax.handler import *
+from xml.sax.saxutils import *
+from xml.sax.xmlreader import *
+from xml.parsers.expat import *
+
+# Internet Protocols and Support
+from webbrowser import *
+from cgi import *
+from cgitb import *
+from wsgiref import *
+from urllib import *
+from urllib.request import *
+from urllib.response import *
+from urllib.parse import *
+from urllib.error import *
+from urllib.robotparser import *
+from http import *
+from http.client import *
+from ftplib import *
+from poplib import *
+from imaplib import *
+from nntplib import *
+from smtplib import *
+from smtpd import *
+from telnetlib import *
+from uuid import *
+from socketserver import *
+from http.server import *
+from http.cookies import *
+from http.cookiejar import *
+from xmlrpc import *
+from xmlrpc.client import *
+from xmlrpc.server import *
+from ipaddress import *
+
+# Multimedia Services
+from audioop import *
+from aifc import *
+from sunau import *
+from wave import *
+from chunk import *
+from colorsys import *
+from imghdr import *
+from sndhdr import *
+# from ossaudiodev
+
+# Internationalization
+from gettext import *
+from locale import *
+
+# Program Frameworks
+from turtle import *
+from cmd import *
+from shlex import *
+
+# Graphical User Interfaces with Tk
+from tkinter import *
+from tkinter.ttk import *
+from tkinter.tix import *
+from tkinter.scrolledtext import *
+
+# Development Tools
+from typing import *
+from pydoc import *
+from doctest import *
+from unittest import *
+from unittest.mock import *
+from test import *
+from test.support import *
+
+# Debugging and Profiling
+from bdb import *
+from faulthandler import *
+from pdb import *
+# The Python Profilers
+from timeit import *
+from trace import *
+from tracemalloc import *
+
+# Software Packaging and Distribution
+from distutils import *
+from ensurepip import *
+from venv import *
+from zipapp import *
+
+# Python Runtime Services
+from sys import *
+from sysconfig import *
+from builtins import *
+from warnings import *
+from contextlib import *
+from abc import *
+from atexit import *
+from traceback import *
+
+from gc import *
+from inspect import *
+from site import *
+# from fpectl
+
+# Custom Python Interpreters
+from code import *
+from codeop import *
+
+# Importing Modules
+from zipimport import *
+from pkgutil import *
+from modulefinder import *
+from runpy import *
+from importlib import *
+
+# Python Language Services
+from parser import *
+from ast import *
+from symtable import *
+from symbol import *
+from token import *
+from keyword import *
+from tokenize import *
+from tabnanny import *
+from pyclbr import *
+from py_compile import *
+from compileall import *
+from dis import *
+from pickletools import *
+
+# Miscellaneous Tools
+from formatter import *
